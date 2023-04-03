ade53f3bae7e1a2299559979133076c55c3f6f8d
Akash Gupta <akashgupta7363@gmail.com>
2023-04-03 10:55:32 +0530

    added schema and creating new task sections

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const dbConnect =require('./db/connect');
 const express=require('express');
 const app=express();
--- conrollers/tasks.js
+++ conrollers/tasks.js
@@ -1,19 +1,25 @@
-
+const Task=require('../models/tasks')
 
 const getAllTasks=(req,res)=>{
-   res.send('All Tasks')
+   res.json(req.body)
 }
-const postNewTask=(req,res)=>{
-   res.send('posting new tasks')
+const postNewTask= async (req,res)=>{
+   try {
+      const task= await Task.create(req.body)
+      res.status(201).json({task})
+      
+   } catch (error) {
+      res.status(500).json({msg : error})
+   }
+}
+const viewTask=(req,res)=>{
+   res.json({id : req.params.id})
 }
 const updateTask=(req,res)=>{
    res.send('updating tasks')
 }
 const deleteTask=(req,res)=>{
    res.send('deleting tasks')
-}
-const viewTask=(req,res)=>{
-   res.send('viewing single tasks')
 }
 
 
